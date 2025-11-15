64283e4bb063c6ba701e3615e4a3a0c5fd2bfec1
Abdelrahman Nasser <abdelrahmanhydara@gmail.com>
2025-11-15 20:50:01 +0200

    - install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopx_dashboard/core/theme/logic/theme_cubit.dart';
 import 'package:shopx_dashboard/shpox_dashboard.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
       BlocProvider(
           create: (_) => ThemeCubit(),
